0493219122bf780957360e48420c28b8d4af56cf
leonardotadeufss <leonardotadeusg@gmail.com>
2022-11-07 21:13:40 -0300

    Add next-pwa and aditional configs

--- next.config.js
+++ next.config.js
@@ -1,14 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const isProd = process.env.NODE_ENV === 'production'
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: !isProd
+})
+
+module.exports = withPWA({
   reactStrictMode: true,
   swcMinify: true
-}
-
-module.exports = nextConfig
-
-module.exports = {
-  // ...
-  experimental: {
-    forceSwcTransforms: true
-  }
-}
+})
